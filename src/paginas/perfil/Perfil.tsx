ac33f81dde23653a979c07f05b2cf4996d501384
Patrícia Silva <86084169+ppfsil@users.noreply.github.com>
2023-08-14 09:46:22 -0300

    Quinta Atual do dia

--- src/paginas/perfil/Perfil.tsx
+++ src/paginas/perfil/Perfil.tsx
@@ -18,7 +18,7 @@ function Perfil() {
     <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
       <img className='w-full h-72 object-cover border-b-8 border-white' src='https://i.pinimg.com/564x/9c/30/27/9c3027bf01d3a9f1b6c9140384d49c3f.jpg' alt="Capa do Perfil" />
       <img src ='https://i.pinimg.com/564x/dd/46/cd/dd46cddef7b8dc4bb734e847395edd35.jpg' alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
-      <div className="relative mt-[-6rem] h-72 flex flex-col bg-sky-500 text-white text-2xl items-center justify-center">
+      <div className="relative mt-[-6rem] h-72 flex flex-col bg-yellow-500 text-white text-2xl items-center justify-center">
         <p>Nome: {usuario.nome} </p>
         <p>Email: {usuario.usuario}</p>
       </div>
